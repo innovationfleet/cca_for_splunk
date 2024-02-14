ad42c8043ea2622745f60454c75de3352f8634ed
AlexanderAdler1 <alexander.adler@orangecyberdefense.com>
2024-02-14 11:37:24 +0100

    Standard Data Onboarding is a steamlined and simplified version of CCA Onboarding                 and are now available in the open source version.                 *  All onboarding apps needs to be placed in splunk/etc/apps/selectable or                     splunk/etc/apps/versioned directories.                 *  Onboarding group_vars files has been simplified, uses sections for selected_apps                     and versioned_apps only. Updated template files are available in                     templates/onboarding_template/environments/ENVIRONMENT_NAME/group_vars.                 *  Directories in onboarding repo for splunk/etc/manager-apps,                     splunk/etc/deployment-apps and splunk/etc/shcluster/apps are no longer used in                     standard data onboarding and can safely be removed if available in a onboarding                     repo.

--- templates/onboarding_template/splunk/etc/apps/versioned/build_appFile_from_splunk_app.sh
+++ templates/onboarding_template/splunk/etc/apps/versioned/build_appFile_from_splunk_app.sh
@@ -9,19 +9,41 @@
 # to make the process a bit easier.
 
 # Validate if the file exists
-test -f ${1} && echo "File ${1} is OK" || echo "File ${1} not found"
+if [[ -n ${1} ]] ; then
+
+  test -f ${1} && echo "File ${1} is OK" || echo "File ${1} not found"
+
+else
+
+  echo "File not specified"
+  exit 1
+
+fi
 
 # Set variables from tgz file
-DirName=$(tar tvf ${1} | awk '{ print $6 }' | cut -d "/" -f1 | sort -u )
-AppVersion=$(echo ${1} | cut -d"_" -f2 | cut -d"." -f1 )
+DirName=$(tar tf ${1} | awk -F"/" '{ print $1 }' | sort -u )
 
 if [[ $(echo ${DirName} | egrep -o " " | wc -l) -eq 0 ]] ; then
 
-  AppFile=$(echo "appFile-${DirName}_v${AppVersion}")
-
   if [[ ! -d ${AppFile} ]] ; then
 
-    tar -xvzf ${1} > /dev/null
+    tar -xzf ${1} > /dev/null
+
+    unset version
+    source <(grep version ${DirName}/default/app.conf | grep = | sed -e 's/[[:space:]]//g' | sed 's/\.//g')
+    if [[ -n ${version} ]] ; then
+
+      # Use the version from apps app.conf file
+      AppVersion=$version
+      AppFile=$(echo "appFile-${DirName}_v${AppVersion}")
+
+    else
+
+      # Fallback to use the version from the file name if app.conf didn't have a version
+      AppVersion=$(echo ${1} | cut -d"_" -f2 | cut -d"." -f1 )
+      AppFile=$(echo "appFile-${DirName}_v${AppVersion}")
+
+    fi
 
     mv ${DirName} ${AppFile}
 
